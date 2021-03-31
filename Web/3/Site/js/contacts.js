e819a8abd805c75e544af8ecb9ed7641450507d0
mratarusama <marinin.i@bk.ru>
2021-04-01 01:56:42 +0300

    3d started

---
+++ Web/3/Site/js/contacts.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+  let form = document.forms[0];
+  form.fio.required = true;
+  form.fio.pattern = '([а-яА-яa-zA-Z]{2,} [а-яА-яa-zA-Z]{2,} [а-яА-яa-zA-Z]{2,})';
+
+  for(let item of form.elements){
+    item.oninvalid = (invalid) => {invalid.target.focus();};
+  }
+
+  form.phone.pattern = '^\\+((\\d{11})|(\\d{9}))';
+  form.phone.value = '+';
+  form.phone.addEventListener('keydown', (event) => {
+    console.log(event);
+    let key = event.key;
+    let input = event.target;
+
+    if(!isNaN(parseInt(key))){
+      if(input.value.length === 1){
+        if(key !== 7 && key !== 3){
+          event.preventDefault();
+        }
+      } else if (input.value.length === 12){
+        event.preventDefault();
+      }
+
+      let startPos = input.selectionStart;
+      let endPos = input.selectionEnd;
+      if(startPos !== endPos) {
+        if(startPos === 0){
+          if(key === 7 || key === 3)
+            input.value = '+' + key + input.value.slice(endPos);
+          event.preventDefault();
+        }
+      }
+    } else {
+      if(key === "Backspace") {
+        if (input.value.length < 2)
+          event.preventDefault();
+        else
+          event.preventDefault();
+      }
+    }
+  });
+};
---
+++ Web/3/Site/js/photos.js
@@ -0,0 +1,77 @@
+const fotos = [
+  './img/cat1.png',
+  './img/cat2.png',
+  './img/cat3.png',
+  './img/cat4.png',
+  './img/cat5.png',
+  './img/cat6.png',
+  './img/cat7.png',
+  './img/cat8.png',
+  './img/cat9.png',
+  './img/cat10.png',
+  './img/cat11.png',
+  './img/cat12.png',
+  './img/cat13.png',
+  './img/cat14.png',
+  './img/cat15.png'
+];
+const titles = [
+  'Картинка 1',
+  'Картинка 2',
+  'Картинка 3',
+  'Картинка 4',
+  'Картинка 5',
+  'Картинка 6',
+  'Картинка 7',
+  'Картинка 8',
+  'Картинка 9',
+  'Картинка 10',
+  'Картинка 11',
+  'Картинка 12',
+  'Картинка 13',
+  'Картинка 14',
+  'Картинка 15'
+];
+
+let build_photo = (url, alt, title, caption) => {
+  let fig = document.createElement('figure');
+  let img = document.createElement('img');
+  let cap = document.createElement('figcaption');
+
+  img.src = url;
+  img.alt = alt;
+  img.title = title;
+
+  cap.innerText = caption;
+  cap.classList.add('picDesc')
+
+  fig.append(img);
+  fig.append(cap);
+  return fig;
+};
+
+let insert_photos = (photo, title, count_in_row) => {
+  let photo_count = photo.length;
+  let title_count = title.length;
+  if(photo_count !== title_count){
+    throw 'Different number of objects in arrays';
+  }
+
+  let content = document.createElement('div');
+  content.id = 'content';
+  let row = '';
+  for(let i = 0; i < photo_count; i++) {
+    if(i % count_in_row === 0){
+      content.append(row);
+      row = document.createElement('div');
+      row.classList.add('row');
+    }
+    row.append(build_photo(photo[i], title[i], title[i], title[i]));
+  }
+  content.append(row);
+  document.body.append(content);
+};
+
+window.onload = () => {
+  insert_photos(fotos, titles, 6);
+};
