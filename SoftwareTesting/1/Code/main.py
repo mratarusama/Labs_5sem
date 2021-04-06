d1de6ba7e515b76ea53bf8cbc5e191def0d58b9f
mratarusama <marinin.i@bk.ru>
2021-04-06 20:23:29 +0300

    fix ReplaceAllAfterDiez

--- SoftwareTesting/1/Code/main.py
+++ SoftwareTesting/1/Code/main.py
@@ -8,8 +8,15 @@ def PositiveD_3x3(matr):
 
 
 def ReplaceAllAfterDiez(s):
-    s = s.split('#')
-    return s[0] + ''.join(['#' + '@' * len(a) for a in s[1:]])
+    try:
+        first_in = s.index('#') + 1
+        length = len(s)
+        if first_in > length:
+            return s
+        else:
+            return s[0:first_in] + '@' * (length - first_in)
+    except ValueError:
+        return s
 
 
 def FoundMinimalStrFromFile(fName):
