d3eb5ed5c8c7638d23bdcc2ed678fec63ea169df
mratarusama <marinin.i@bk.ru>
2021-04-06 20:35:24 +0300

    add tests

--- SoftwareTesting/1/Code/tests.py
+++ SoftwareTesting/1/Code/tests.py
@@ -50,9 +50,27 @@ class testPositiveD_3x3(unittest.TestCase):
 
 
 class testStringReplacer(unittest.TestCase):
+    def testOneSymbolString(self):
+        s = 'A'
+        expected = 'A'
+
+        actual = main.ReplaceAllAfterDiez(s);
+
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+
     def testEmptyString(self):
         s = ''
         expected = ''
+
+        actual = main.ReplaceAllAfterDiez(s);
+
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+                         
+    def testAllDiezString(self):
+        s = '##########'
+        expected = '#@@@@@@@@@'
 
         actual = main.ReplaceAllAfterDiez(s);
 
@@ -79,7 +97,25 @@ class testStringReplacer(unittest.TestCase):
 
     def testStringWithManyDiez(self):
         s = 'Hel#o,#world!'
-        expected = 'Hel#@@#@@@@@@'
+        expected = 'Hel#@@@@@@@@@'
+
+        actual = main.ReplaceAllAfterDiez(s);
+
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+    
+    def testStringFirstDiez(self):
+        s = '#ello, world!'
+        expected = '#@@@@@@@@@@@@'
+
+        actual = main.ReplaceAllAfterDiez(s);
+
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+                         
+    def testStringLastDiez(self):
+        s = 'Hello, world#'
+        expected = 'Hello, world#'
 
         actual = main.ReplaceAllAfterDiez(s);
 
@@ -88,6 +124,21 @@ class testStringReplacer(unittest.TestCase):
 
 
 class testFoundMinimalStrInFile(unittest.TestCase):
+    def testOneString(self):
+        fName = 'testEqual.txt'
+        strDat = ['12345']
+        with open(fName, 'w') as f:
+            for s in strDat:
+                f.write(s + '\n')
+
+        expected = strDat[0]
+        actual = main.FoundMinimalStrFromFile(fName)
+
+        os.remove(fName);
+
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+                         
     def testEmptyFile(self):
         fName = 'testEmpty.txt'
         with open(fName, 'w') as f:
@@ -117,6 +168,36 @@ class testFoundMinimalStrInFile(unittest.TestCase):
         self.assertEqual(expected, actual,
                          f'Expected: {expected}\nGot: {actual}')
 
+    def testFirstMin(self):
+        fName = 'testNormal.txt'
+        strDat = ['12345', '6722152890',
+                  '521', '5123121']
+        with open(fName, 'w') as f:
+            for s in strDat:
+                f.write(s + '\n')
+
+        expected = strDat[2]
+        actual = main.FoundMinimalStrFromFile(fName)
+
+        os.remove(fName);
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+                        
+    def testLastMin(self):
+        fName = 'testNormal.txt'
+        strDat = ['12345', '6722152890',
+                  '521', '5123121']
+        with open(fName, 'w') as f:
+            for s in strDat:
+                f.write(s + '\n')
+
+        expected = strDat[2]
+        actual = main.FoundMinimalStrFromFile(fName)
+
+        os.remove(fName);
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
+                         
     def testNormal(self):
         fName = 'testNormal.txt'
         strDat = ['12345', '6722152890',
@@ -132,6 +213,20 @@ class testFoundMinimalStrInFile(unittest.TestCase):
         self.assertEqual(expected, actual,
                          f'Expected: {expected}\nGot: {actual}')
 
+    def testAllEmpty(self):
+        fName = 'testNormal.txt'
+        strDat = ['', '',
+                  '', '']
+        with open(fName, 'w') as f:
+            for s in strDat:
+                f.write(s + '\n')
+
+        expected = strDat[0]
+        actual = main.FoundMinimalStrFromFile(fName)
+
+        os.remove(fName);
+        self.assertEqual(expected, actual,
+                         f'Expected: {expected}\nGot: {actual}')
 
 if __name__ == '__main__':
     unittest.main()
